1dd18e177e9ddbec12deba940eb0e47239541588
dhanushpittala <dhanushpittala05@gmail.com>
2024-11-20 19:34:01 +0530

    Prediction Pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage1_dataIngestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage2_DataTransformation_pipeline import DataTransformationTrainingPipeline
 from src.textSummarizer.pipeline.stage3_modeltrainer_pipeline import ModelTrainerTrainingPipeline
+from src.textSummarizer.pipeline.stage4_modelevaluation_pipeline import ModelEvaluationTrainingPipeline
 
 STAGE_NAME="Data Ingestion stage"
 
@@ -31,6 +32,17 @@ try:
     logger.info(f"stage {STAGE_NAME} initiated")
     model_trainer_pipeline=ModelTrainerTrainingPipeline()
     model_trainer_pipeline.initiate_data_transformation()
+    logger.info("Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Model Evaluation stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    model_trainer_pipeline=ModelEvaluationTrainingPipeline()
+    model_trainer_pipeline.initiate_model_evaluation()
     logger.info("Stage {STAGE_NAME} Completed")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/pipeline/stage3_modeltrainer_pipeline.py
+++ src/textSummarizer/pipeline/stage3_modeltrainer_pipeline.py
@@ -5,6 +5,7 @@ from src.textSummarizer.logging import logger
 class ModelTrainerTrainingPipeline:
     def __init__(self):
         pass
+    
     def initiate_model_trainer(self):
         config = ConfigurationManager()
         model_trainer_config = config.get_model_trainer_config()
